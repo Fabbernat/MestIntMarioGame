6231ce0324a780bac2ec5f4d140b7d84264736d9
Fabian Bernat <fabbernat@gmail.com>
2025-11-08 16:09:32 +0100

    Fixed this: Szerintem rossz helyen keresi a fájlt, mert van a jár-on kívül is egy game/mario/players mappa, és a jár-on belül is egy game.mario.players package, amit szerintem a java -jar game_engine.jar 60 game.mario.MarioGame 1234567890 1000 game.mario.players.Agent parancs a jar-on belül fogja megpróbálni megkeresni, ahol nyilván nem fogja megtalálni, mert nem a C:\Users\Bernát\IdeaProjects\MestIntMarioGame\src\game_engine.jar-on belül van, hanem a C:\Users\Bernát\IdeaProjects\MestIntMarioGame\src\game\mario\players-en belül. A package-ek teljesen rendben vannak az Agent.javában, de hogyha máshova helyezem (a járon kívül), akkor is ugyanúgy a járon belül keresi a fordítóprogram ezzel a java -jar game_engine.jar 60 game.mario.MarioGame 1234567890 1000 game.mario.players.Agent paranccsal. Hogyan kell módosítani a futtatást (akár PS paranccsal, akár az IntelliJ Idea egy builtin feature-jével), hogy értse a Java, hol kell keresse a fájlomat?

--- src/game/mario/players/Agent.java
+++ src/game/mario/players/Agent.java
@@ -1,22 +1,26 @@
+package game.mario.players;
+
 import game.mario.Direction;
 import game.mario.MarioGame;
 import game.mario.MarioPlayer;
 import game.mario.utils.Mario;
-import utils.State;
+import game.mario.utils.MarioState;
 
 import java.util.Random;
 
 public class Agent extends MarioPlayer {
 
-  public Agent(int color, Random random, State marioState) {
+  public Agent(int color, Random random, MarioState marioState) {
     super(color, random, marioState);
   }
+
   @Override
   public Direction getDirection(long remainingTime) {
-    Direction action = new Direction(MarioGame.DIRECTIONS[random.nextInt(MarioGame.DIRECTIONS.length-1)]);
+    Direction action = new Direction(MarioGame.DIRECTIONS[random.nextInt(MarioGame.DIRECTIONS.length - 1)]);
     state.apply(action);
     return action;
   }
+
 
   // Visszatér egy Directionnal a Mario vízszintes és függőleges sebessége alapján
   private Direction AStarAlgorithm(Mario mario) {
