7929a65ab2c5220559579b77d9d31b6028fa7a03
Fabian Bernat <fabbernat@gmail.com>
2025-11-08 15:31:18 +0100

    Agent.java code replaced with RightUpPlayer code

--- src/Agent.java
+++ src/Agent.java
@@ -2,43 +2,19 @@ import game.mario.Direction;
 import game.mario.MarioGame;
 import game.mario.MarioPlayer;
 import game.mario.utils.Mario;
-import game.mario.utils.MarioState;
 import utils.State;
 
 import java.util.Random;
 
-
-
 public class Agent extends MarioPlayer {
-  private int[][] map;
-  Mario mario;
-  Random random = new Random();
 
   public Agent(int color, Random random, State marioState) {
     super(color, random, marioState);
-    this.map = new int[13][100];
-    this.mario = new Mario(6, 10);
   }
   @Override
   public Direction getDirection(long remainingTime) {
-    System.out.println("Hello World");
-
-
-    Direction action = new Direction(MarioGame.DIRECTIONS[random.nextInt(MarioGame.DIRECTIONS.length)]);
-
-    State state = new State(map, mario);
-    MarioState s0 = new MarioState(state);
-
-    s0.apply(new Direction(MarioGame.DIRECTIONS[3]));
-
-    // heurisztikát készítünk?
-    double heur0 = s0.score;
-    System.out.println(action);
+    Direction action = new Direction(MarioGame.DIRECTIONS[random.nextInt(MarioGame.DIRECTIONS.length-1)]);
     state.apply(action);
-
-    // lefuttatjuk az A* algoritmust a Mario jelenlegi helyzete alapján
-    action = AStarAlgorithm(mario);
-
     return action;
   }
 
