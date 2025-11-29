8be456c412a272251bf48ae2df27a42d747d6846
Fabian Bernat <fabbernat@gmail.com>
2025-11-29 12:38:48 +0100

    v8.2.2: KA számára sokkal olvashatóbbá tétel

--- src/Agent.java
+++ src/Agent.java
@@ -85,16 +85,16 @@ public class Agent extends MarioPlayer {
     return hatarertek;
   }
 
-  Double closestBlock(MarioState var1) {
+  Double closestBlock(MarioState state) {
     double var2 = 1000.0F;
     Object var4 = null;
 
     try {
-      Point var5 = new Point(var1.mario.j, var1.mario.i);
+      Point var5 = new Point(state.mario.j, state.mario.i);
 
-      for (int var6 = (int) var1.mario.i; var6 > (int) var1.mario.i - 4; --var6) {
-        for (int var7 = (int) var1.mario.j; var7 < (int) var1.mario.j + 4; ++var7) {
-          if (var1.map[var6][var7] == 3) {
+      for (int var6 = (int) state.mario.i; var6 > (int) state.mario.i - 4; --var6) {
+        for (int var7 = (int) state.mario.j; var7 < (int) state.mario.j + 4; ++var7) {
+          if (state.map[var6][var7] == 3) {
             Point var8 = new Point(var7, var6);
             double var9 = (var8.x - var5.x) * (var8.x - var5.x);
             double var11 = (var8.y - var5.y) * (var8.y - var5.y);
@@ -111,92 +111,92 @@ public class Agent extends MarioPlayer {
     return var2;
   }
 
-  ArrayList<Direction> converter(String var1) {
-    ArrayList var2 = new ArrayList();
-    byte var3 = 4;
-    if (var1.contains("S")) {
-      var3 = 2;
+  ArrayList<Direction> converter(String s) {
+    ArrayList<Direction> list = new ArrayList<>();
+    byte b = 4;
+    if (s.contains("S")) {
+      b = 2;
     }
 
-    if (var1.contains("V")) {
-      var3 = 1;
+    if (s.contains("V")) {
+      b = 1;
     }
 
-    if (var1.contains("R")) {
-      for (int var4 = 0; var4 < var3; ++var4) {
-        var2.add(new Direction(0));
+    if (s.contains("R")) {
+      for (int var4 = 0; var4 < b; ++var4) {
+        list.add(new Direction(0));
       }
     }
 
-    if (var1.contains("L")) {
-      for (int var5 = 0; var5 < var3; ++var5) {
-        var2.add(new Direction(2));
+    if (s.contains("L")) {
+      for (int var5 = 0; var5 < b; ++var5) {
+        list.add(new Direction(2));
       }
     }
 
-    if (var1.contains("U") || var1.equals("UP")) {
-      for (int var6 = 0; var6 < var3; ++var6) {
-        var2.add(new Direction(1));
+    if (s.contains("U") || s.equals("UP")) {
+      for (int var6 = 0; var6 < b; ++var6) {
+        list.add(new Direction(1));
       }
     }
 
-    if (var1.contains("N") || var1.isEmpty()) {
-      var2.add(null);
+    if (s.contains("N") || s.isEmpty()) {
+      list.add(null);
     }
 
-    return var2;
+    return list;
   }
 
-  List<Direction> aStar(MarioState var1) {
-    PriorityQueue var2 = new PriorityQueue();
-    HashMap var3 = new HashMap();
-    Node var4 = new Node(var1.distance, 100.0F, (Node) null, var1, (String) null);
-    var2.add(var4);
+  List<Direction> aStar(MarioState state) {
+    PriorityQueue queue = new PriorityQueue();
+    HashMap map = new HashMap();
+    Node addedNode = new Node(state.distance, 100.0F, null, state, null);
+    queue.add(addedNode);
 
-    while (!var2.isEmpty()) {
+    while (!queue.isEmpty()) {
       ++this.depth;
-      Node var5 = (Node) var2.poll();
-      if (this.isGoal(var5) || this.depth > this.maxDepth) {
+      Node polledNode = (Node) queue.poll();
+      if (this.isGoal(polledNode) || this.depth > this.maxDepth) {
         this.depth = 0.0F;
-        return this.path(var5);
+        return this.path(polledNode);
       }
 
-      double var6 = (double) 101.0F - var5.state.mario.j;
+      double szam = (double) 101.0F - polledNode.state.mario.j;
 
-      for (String var11 : this.Actions) {
-        MarioState var12 = new MarioState(var5.state);
-        double var13 = 1.0F;
-        var13 += Math.min(1.0F, (double) 1.0F / this.closestHole(var12));
+      for (String akcio : this.Actions) {
+        MarioState aktState = new MarioState(polledNode.state);
+        double f = 1.0F;
+        f += Math.min(1.0F, (double) 1.0F / this.closestHole(aktState));
 
         try {
-          int[][] var15 = new int[3][3];
+          int[][] gameMap = new int[3][3];
 
-          for (int var16 = (int) var12.mario.i - 1; var16 > (int) var12.mario.i + 1; ++var16) {
-            for (int var17 = (int) var12.mario.j - 1; var17 > (int) var12.mario.j + 1; ++var17) {
-              var15[var16][var17] = var12.map[var16][var17];
+          for (int var16 = (int) aktState.mario.i - 1; var16 > (int) aktState.mario.i + 1; ++var16) {
+            for (int var17 = (int) aktState.mario.j - 1; var17 > (int) aktState.mario.j + 1; ++var17) {
+              gameMap[var16][var17] = aktState.map[var16][var17];
             }
           }
 
-          if (var15[0][1] == 1 && var15[0][2] == 1 || var15[0][1] == 2 && var15[0][2] == 2) {
-            ++var13;
+          if (gameMap[0][1] == 1 && gameMap[0][2] == 1 || gameMap[0][1] == 2 && gameMap[0][2] == 2) {
+            ++f;
           }
 
-          if (var12.mario.i >= (double) 12.0F) {
-            if (var15[2][1] == 0 || var15[2][2] == 0 || var15[2][0] == 0) {
-              ++var13;
+          if (aktState.mario.i >= (double) 12.0F) {
+            if (gameMap[2][1] == 0 || gameMap[2][2] == 0 || gameMap[2][0] == 0) {
+              ++f;
             }
 
-            if (var15[2][1] != 0 && var15[2][2] != 0) {
-              var13 -= 2.0F;
+            if (gameMap[2][1] != 0 && gameMap[2][2] != 0) {
+              f -= 2.0F;
             }
           }
         } catch (Exception var18) {
         }
 
         try {
-          for (Direction var25 : this.converter(var11)) {
-            if (!var12.apply(var25)) {
-              var13 += 100.0F;
+          for (Direction var25 : this.converter(akcio)) {
+            if (!aktState.apply(var25)) {
+              f += 100.0F;
             }
           }
         } catch (Exception var19) {
@@ -205,39 +205,39 @@ public class Agent extends MarioPlayer {
         try {
           int[][] var23 = new int[3][3];
 
-          for (int var26 = (int) var12.mario.i - 1; var26 > (int) var12.mario.i + 1; ++var26) {
-            for (int var28 = (int) var12.mario.j - 1; var28 > (int) var12.mario.j + 1; ++var28) {
-              var23[var26][var28] = var12.map[var26][var28];
+          for (int var26 = (int) aktState.mario.i - 1; var26 > (int) aktState.mario.i + 1; ++var26) {
+            for (int var28 = (int) aktState.mario.j - 1; var28 > (int) aktState.mario.j + 1; ++var28) {
+              var23[var26][var28] = aktState.map[var26][var28];
             }
           }
 
-          if (var12.mario.i >= (double) 11.0F) {
+          if (aktState.mario.i >= (double) 11.0F) {
             if (var23[2][1] == 0 || var23[2][2] == 0) {
-              var13 += 10.0F;
+              f += 10.0F;
             }
 
             if (var23[2][1] != 0 || var23[2][2] != 0 || var23[2][0] != 0) {
-              var13 -= 10.0F;
+              f -= 10.0F;
             }
           }
         } catch (Exception ignored) {
         }
 
-        if (var6 >= var12.distance) {
-          var13 += 3.0F;
+        if (szam >= aktState.distance) {
+          f += 3.0F;
         }
 
-        if (var12.score < (double) 1000.0F) {
-          var13 += 3.0F;
-        } else if (var12.score < (double) 2000.0F) {
-          ++var13;
+        if (aktState.score < (double) 1000.0F) {
+          f += 3.0F;
+        } else if (aktState.score < (double) 2000.0F) {
+          ++f;
         }
 
-        Node var24 = new Node(var5.g + var13, this.heurisztika(var12), var5, var12, var11);
-        String var27 = var12.mario.i + ":" + var12.mario.j + ":" + var12.isInAir;
-        if (!var3.containsKey(var27) || var24.f < (Double) var3.get(var27)) {
-          var3.put(var27, var24.f);
-          var2.add(var24);
+        Node var24 = new Node(polledNode.g + f, this.heurisztika(aktState), polledNode, aktState, akcio);
+        String var27 = aktState.mario.i + ":" + aktState.mario.j + ":" + aktState.isInAir;
+        if (!map.containsKey(var27) || var24.f < (Double) map.get(var27)) {
+          map.put(var27, var24.f);
+          queue.add(var24);
         }
       }
     }
